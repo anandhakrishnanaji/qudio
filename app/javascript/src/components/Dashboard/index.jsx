3fad01935a39b6b6efc874d1d9a0a79925b57833
Anandha Krishnan <anandha.krishnan@bigbinary.com>
2025-08-05 19:55:49 +0530

    Initial commit

---
+++ app/javascript/src/components/Dashboard/index.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+import Sidebar from "components/commons/Sidebar";
+import {
+  DASHBOARD_ROUTES,
+  NOTES_PATH,
+  DASHBOARD_PATH,
+} from "components/routeConstants";
+import { Route, Redirect, Switch } from "react-router-dom";
+
+const Dashboard = () => (
+  <div className="flex h-screen w-full">
+    <Sidebar />
+    <Switch>
+      {DASHBOARD_ROUTES.map(({ path, component }) => (
+        <Route exact component={component} key={path} path={path} />
+      ))}
+      <Redirect from={DASHBOARD_PATH} to={NOTES_PATH} />
+    </Switch>
+  </div>
+);
+
+export default Dashboard;
